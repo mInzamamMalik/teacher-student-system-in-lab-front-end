ec4ea4467204cfff98a8944d50f3ac5dcdb03560
inzamam malik <malikasinger1@gmail.com>
2015-12-02 20:36:09 +0500

    trying

--- views/login/login.js
+++ views/login/login.js
@@ -1,7 +1,13 @@
+
+
 angular.module("myApp")
-    .controller("loginController",function($scope){
-        $scope.abc = function(){
-            alert();
+    .controller("loginController",function($scope,$http){
+
+        $scope.login = function(){
+            $http.body.email = $scope.user.email;
+            $http.body.password = $scope.user.password;
+
+            $http.get("http://localhost:3000/login");
 
         }
     });
